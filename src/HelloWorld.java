7b2c73fc3a601897f41eac3ea77258b644997d30
xxl <1246786087@qq.com>
2020-11-04 11:23:45 +0800

    TEST

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @AUTHOR Xiaxiaoli
+ * @create 2020-11-04-11:09
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
